c063535f8991593ff84d5d39e52fc9f2ebe9611d
DELL <ssecerhalill@gmail.com>
2021-06-09 12:53:12 +0300

    maini güncelledim 12,53

--- app/src/main/java/com/example/tatil/MainActivity.java
+++ app/src/main/java/com/example/tatil/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {// MAİN ACTİVİTY navigasyon selected item yorumy
                 switch (item.getItemId()){
                     case R.id.nav_home:
                         selectorFragment = new HomeFragment();
